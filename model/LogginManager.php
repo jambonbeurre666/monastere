29f6f0373abe331b4bbdff455e63579545e0c063
david.quartino <david.quartino@gmail.com>
2018-03-21 13:54:29 +0100

    Mise en forme du code

--- model/LogginManager.php
+++ model/LogginManager.php
@@ -5,8 +5,7 @@ class LogginManager extends Manager
 {
     public function getUser($mail)
     {
-
-        $req = $this->conn->prepare('SELECT nom, prenom, motdepasse FROM users WHERE mail = ?');
+        $req = $this->conn->prepare('SELECT nom, prenom, motdepasse, mail FROM users WHERE mail = ?');
         $req->execute(array($mail));
         $user = $req->fetch();
         return $user;
