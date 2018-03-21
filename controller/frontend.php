b59ad9f603c072f3dbf25d7b265a58d5831729e0
david.quartino <david.quartino@gmail.com>
2018-03-21 13:46:53 +0100

    Ajout de la method isLoogged qui verifie si l'user est loggé. Ajout de la method disconnect pour se logout. Ajout de la method verifyUser qui verifie l'user et le mdp

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,11 +1,42 @@
 <?php
 
+function isLogged() {
+
+    if(isset($_GET['action'])) {
+        if($action !== 'loggin' || $action !== 'disconnect') {
+            if(!isset($_SESSION['logged'])) {
+                header('location:index.php');
+            } elseif($_SESSION['logged'] !== true) {
+                header('location:index.php');
+            }
+        }
+    } else {
+        return true; 
+    }
+}
+
 function homeController()
 {
+    require('view/home_view.php');
+}
+
+function verifyUser() {
     require_once('model/LogginManager.php');
-
     $logginManager = new LogginManager();
-    $user = $logginManager->getUser("admin@abi.fr");
+    $user = $logginManager->getUser($_POST['mail']);
+    if(isset($_POST['mail']) && isset($_POST['pass'])) {
+        if($_POST['mail'] === $user['mail'] && password_verify($_POST['pass'], $user['motdepasse']) === true) {
+            $_SESSION['logged'] = true;
+            header('location:index.php');
+        } else {
+            throw new Exception("La combinaison email/mot de passe est incorrect");
+        }
+    } else {
+        throw new Exception("Merci de renseigner tous les champs");
+    }
+}
 
-    require('view/homeView.php');
+function disconnect() {
+    session_destroy();
+    header('location:index.php');
 }
