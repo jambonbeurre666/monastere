7db33914e922ee91545f48027e7bdcf0400cd4e2
david.quartino <david.quartino@gmail.com>
2018-03-26 10:07:48 +0200

    Modification de la methos isLogged et changement de chemin vers fichier dao

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,17 +1,16 @@
 <?php
 
-function isLogged() {
-
-    if(isset($_GET['action'])) {
-        if($action !== 'loggin' || $action !== 'disconnect') {
-            if(!isset($_SESSION['logged'])) {
+function isLogged()
+{
+    if (isset($_GET['action'])) {
+        $authorize = array("loggin", "disconnect");
+        if (!in_array($_GET['action'], $authorize)) {
+            if (!isset($_SESSION['logged'])) {
                 header('location:index.php');
-            } elseif($_SESSION['logged'] !== true) {
+            } elseif ($_SESSION['logged'] !== true) {
                 header('location:index.php');
             }
         }
-    } else {
-        return true; 
     }
 }
 
@@ -20,13 +19,16 @@ function homeController()
     require('view/home_view.php');
 }
 
-function verifyUser() {
-    require_once('model/LogginManager.php');
+function verifyUser()
+{
+    require_once('dao/LogginManager.php');
     $logginManager = new LogginManager();
     $user = $logginManager->getUser($_POST['mail']);
-    if(isset($_POST['mail']) && isset($_POST['pass'])) {
-        if($_POST['mail'] === $user['mail'] && password_verify($_POST['pass'], $user['motdepasse']) === true) {
+    if (isset($_POST['mail']) && isset($_POST['pass'])) {
+        if ($_POST['mail'] === $user['mail'] && password_verify($_POST['pass'], $user['motdepasse']) === true) {
             $_SESSION['logged'] = true;
+            $_SESSION['nom'] = $user['nom'];
+            $_SESSION['prenom'] = $user['prenom'];
             header('location:index.php');
         } else {
             throw new Exception("La combinaison email/mot de passe est incorrect");
@@ -36,7 +38,8 @@ function verifyUser() {
     }
 }
 
-function disconnect() {
+function disconnect()
+{
     session_destroy();
     header('location:index.php');
 }
