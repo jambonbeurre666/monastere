a2b742d63f0d54ce48fd6b4862b8c9cbb1647dcc
thibo martin <thibo06130@hotmail.fr>
2018-03-26 16:01:14 +0200

    Classe_client à vérif

---
+++ Class_Client.php
@@ -0,0 +1,196 @@
+<?php 
+//require(Model.php);
+
+class Client
+{
+    //attributes
+    private $idClient;
+    private $raisonSociale;
+    private $nature;
+    private $type;
+    private $adressemail;
+    private $adresse;
+    private $ville;
+    private $codePostal;
+    private $telephone ;
+    private $ca;
+    private $effectif;
+    private $commentaire;
+
+
+    public function __construct($tel, $post, $email)
+    {
+        $this->setTelephone($tel);
+        $this->setCodePostal($post);
+        $this->setAdressemail($email);
+    }
+
+
+    //VERIFICATION TELEPHONE
+    /* numeric, decimal passes */
+    private function validate_numeric($telephone)
+    {
+        return is_numeric($telephone);
+    }
+
+    /* digits only, no dots */
+    private function is_digits($telephone)
+    {
+        return !preg_match("/[^0-9]/", $telephone);
+    }
+  
+    private function lengthDigit($digit)
+    {
+        return strlen($digit);
+    }
+    
+  
+   
+      
+    //---------------------------
+
+
+    public function getIdClient()
+    {
+        return $this->idClient;
+    }
+
+    public function setIdClient($idClient)
+    {
+        $this->idClient = $idClient;
+    }
+
+    public function getRaisonSociale()
+    {
+        return $this->raisonSociale;
+    }
+
+    public function setRaisonSociale($raisonSociale)
+    {
+        $this->raisonSociale = $raisonSociale;
+    }
+
+    public function getNature()
+    {
+        return $this->nature;
+    }
+
+    public function setNature($nature)
+    {
+        $this->nature = $nature;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    }
+
+    public function getCodePostal()
+    {
+        return $this->codePostal;
+    }
+
+    public function setCodePostal($codePostal)
+    {
+        $this->codePostal = $codePostal;
+        
+        $check = "/^([0-9]{5})$/";
+
+        if(preg_match($check,$codePostal)===1){
+            $this->codePostal = $codePostal;
+        }
+        else{
+          throw new Exception("Veuillez saisir un code postal correct");
+        }
+      
+
+
+    }
+
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone($telephone)
+    {
+        if ($this->validate_numeric($telephone)&& $this->lengthDigit($telephone)===10 && $this->is_digits($telephone)) {
+            $this->telephone = wordwrap($telephone, 2, " ", true);
+           
+        } 
+        else {
+            throw new Exception("Veuillez saisir un numéro valide");
+        }
+    }
+
+    public function getCa()
+    {
+        return $this->ca;
+    }
+
+    public function setCa($ca)
+    {
+        $this->ca = $ca;
+    }
+
+    public function getEffectif()
+    {
+        return $this->effectif;
+    }
+
+    public function setEffectif($effectif)
+    {
+        $this->effectif = $effectif;
+    }
+
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire($commentaire)
+    {
+        $this->commentaire = $commentaire;
+    }
+    public function getAdressemail(){
+		return $this->adressemail;
+	}
+
+	public function setAdressemail($adressemail){
+      
+        if (filter_var($adressemail, FILTER_VALIDATE_EMAIL)) {
+           
+        $this->adressemail = $adressemail;
+        }
+        else
+        {
+            throw new Exception("Veuillez saisir une adresse mail valide");
+        }
+
+	}
+}
