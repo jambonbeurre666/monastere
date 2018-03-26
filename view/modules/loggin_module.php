b23255b1032f6745872c5afa443da3a60de9082b
david.quartino <david.quartino@gmail.com>
2018-03-26 10:11:36 +0200

    Ajout nom et prénom une fois loggé

--- view/modules/loggin_module.php
+++ view/modules/loggin_module.php
@@ -1,11 +1,16 @@
-<?php if(isset($_SESSION['logged']) && $_SESSION['logged'] === true) { ?>
+<?php if (isset($_SESSION['logged']) && $_SESSION['logged'] === true) {
+    ?>
+    <p>Bienvenue <?= $_SESSION['prenom'] . " " . $_SESSION['nom']; ?></p>
     <a href="index.php?action=disconnect">Déconnexion</a>
-<?php } else { ?>
+<?php
+} else {
+        ?>
     <form action="index.php?action=loggin" method="post">
         <label for="mail">email :</label>
         <input type="text" name="mail">
-        <label for="pass">Mot de passe :
+        <label for="pass">Mot de passe :</label>
         <input type="password" name="pass">
-        <input name="Valider" type="submit" value="Valider" />
+        <input name="Valider" type="submit" value="Valider" class="btn-loggin" />
     </form>
-<?php } ?>
+<?php
+    } ?>
