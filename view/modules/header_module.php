73318d09451da2be3dfb4d9ca27d40405b972b70
david.quartino <david.quartino@gmail.com>
2018-03-26 15:48:55 +0200

    Module header

---
+++ view/modules/header_module.php
@@ -0,0 +1,9 @@
+<header class="mb-5">
+    <div class="pre-header">
+    <div class="container">
+    <img src="../public/images/logo_abi.jpg" alt="Logo_page" title="Accueil" class="logo-main" />
+    <?php require_once('loggin_module.php'); ?>
+    </div>
+    </div>
+    <?php require_once('menu_module.php'); ?>
+</header>
