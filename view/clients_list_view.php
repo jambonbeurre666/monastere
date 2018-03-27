737ed413ceb41c761a05ddb7240e74eda93f7e8b
david.quartino <david.quartino@gmail.com>
2018-03-27 15:58:40 +0200

    Maj bouton view fiche client

--- view/clients_list_view.php
+++ view/clients_list_view.php
@@ -1,5 +1,6 @@
 <?php
-    $title = "Bienvenue sur le site Abi";
+    $title = "Liste des clients";
+    $selected = "list";
     ob_start();
 ?>
 <div class="container">
@@ -16,13 +17,16 @@
     </thead>
     <tbody>
         <?php foreach ($results as $result) {
+            $url = generateRewritedUrl($result['idClient'], $result['RaisonSociale'], 'client', '-');
+
+            
     echo '<tr>';
     echo '<td>'. $result['RaisonSociale'] .'</td>';
     echo '<td>'. $result['Telephone'].'</td>';
     echo '<td>'. $result['MailClient'].'</td>';
     echo '<td>';
     echo '<div class="btn-hover">';
-    echo '<a href="index.php?action=view-customer&id='.$result['idClient'].'" class="btn btn-outline-success"><i class="fas fa-eye"></i></a> ';
+    echo '<a href="'. $url .'" class="btn btn-outline-success"><i class="fas fa-eye"></i></a> ';
     echo '<a href="index.php?action=update-customer&id='.$result['idClient'].'" class="btn btn-outline-warning"><i class="fas fa-pencil-alt"></i></a> ';
     echo '<a href="index.php?action=delete-customer&id='.$result['idClient'].'" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></a>';
     echo '</div>';
