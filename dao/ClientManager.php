f9736bef6b026309c03627d3a42116247d7f6fe0
david.quartino <david.quartino@gmail.com>
2018-03-27 15:57:45 +0200

    Mise à jour requete pour selectionner un client

--- dao/ClientManager.php
+++ dao/ClientManager.php
@@ -6,13 +6,17 @@ class ClientManager extends Manager
 {
     public function getAllCustomers()
     {
-        $req = $this->conn->query('SELECT idClient, RaisonSociale, Telephone, MailClient FROM clients ORDER BY RaisonSociale ASC');
+        $req = $this->conn->query('SELECT idClient, RaisonSociale, Telephone, MailClient FROM `clients` ORDER BY RaisonSociale ASC');
         $results = $req->fetchAll();
         return $results;
     }
 
     public function getCustomer($id)
     {
+        $req = $this->conn->prepare('SELECT * FROM `clients` WHERE idClient = ?');
+        $req->execute(array($id));
+        $result = $req->fetch();
+        return $result;
     }
 
     public function deleteCustomer($id)
