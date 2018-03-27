d57b506f62553affa6ee7932c028a20fee235b09
david.quartino <david.quartino@gmail.com>
2018-03-27 15:57:03 +0200

    Ajout route error et view customer

--- index.php
+++ index.php
@@ -2,17 +2,22 @@
 session_start();
 require('controller/frontend.php');
 isLogged();
-
 try {
     if (isset($_GET['action']) && $_GET['action'] !== "") {
         if ($_GET['action'] === "loggin") {
             verifyUser();
         } elseif ($_GET['action'] === "disconnect") {
             disconnect();
+        } elseif ($_GET['action'] === "error") {
+            pageErreur();
         } elseif ($_GET['action'] === "customers-list") {
             listClient();
+        } elseif ($_GET['action'] === "view-customer") {
+            viewCustomer();
         } else {
-            throw new Exception("{$_GET['action']} n'existe pas !");
+            //throw new Exception("{$_GET['action']} n'existe pas !");
+            http_response_code(404);
+
             //header("location: index.php");
         }
     } else {
@@ -21,3 +26,5 @@ try {
 } catch (Exception $e) {
     echo $e->getMessage();
 }
+
+
