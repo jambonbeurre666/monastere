b435b5b128353c87b626cdee4c24f359a54b137f
david.quartino <david.quartino@gmail.com>
2018-03-29 13:54:55 +0200

    Maj url image

--- view/modules/header_module.php
+++ view/modules/header_module.php
@@ -1,7 +1,7 @@
 <header class="mb-5">
     <div class="pre-header">
     <div class="container">
-    <img src="../public/images/logo_abi.jpg" alt="Logo_page" title="Accueil" class="logo-main" />
+    <img src="/public/images/logo_abi.jpg" alt="Logo_page" title="Accueil" class="logo-main" />
     <?php require_once('loggin_module.php'); ?>
     </div>
     </div>
