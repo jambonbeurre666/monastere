6214223ec8723e36511c1312ad6123cc5b3a4f1b
david.quartino <david.quartino@gmail.com>
2018-04-05 14:02:03 +0200

    Création du module modale

---
+++ view/modules/modale_module.php
@@ -0,0 +1,21 @@
+<!-- Modal -->
+<div class="modal fade" id="deleteConf" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header text-light bg-dark">
+                <h5 class="modal-title">Demande de confirmation</h5>
+                <button type="button" class="close text-light" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Voulez-vous vraiment supprimer le client
+                    <strong class="customers-name"></strong> ?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" data-dismiss="modal">Annuler</button>
+                <a href="#" class="btn btn-danger delete-btn">Supprimer</a>
+            </div>
+        </div>
+    </div>
+</div>
