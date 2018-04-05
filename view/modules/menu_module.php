ca752a3dabfc18788a807933cf8ed3b88402565d
david.quartino <david.quartino@gmail.com>
2018-04-05 13:41:07 +0200

    Maj du module du menu avec le zone de recherche

--- view/modules/menu_module.php
+++ view/modules/menu_module.php
@@ -1,29 +1,40 @@
 <div class="nav-bar bg-primary">
 
-<div class="container">
+  <div class="container">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary pl-0 pr-0">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item <?= ($_SERVER['REQUEST_URI'] === '/') ? 'active' : ''; ?>">
-        <a class="nav-link" href="/">Accueil</a>
-      </li>
-      <li class="nav-item <?= (substr($_SERVER['REQUEST_URI'], 0, strlen('/liste-clients/')) === '/liste-clients/') ? 'active' : ''; ?>">
-        <a class="nav-link" href="/liste-clients/1/">Liste des clients</a>
-      </li>
-      <li class="nav-item <?= ($_SERVER['REQUEST_URI'] === '/ajouter-client/') ? 'active' : ''; ?>">
-        <a class="nav-link" href="/ajouter-client/">Ajouter un clients</a>
-      </li>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item <?= ($_SERVER['REQUEST_URI'] === '/') ? 'active' : ''; ?>">
+            <a class="nav-link" href="/">Accueil</a>
+          </li>
+          <li class="nav-item <?= (substr($_SERVER['REQUEST_URI'], 0, strlen('/liste-clients/')) === '/liste-clients/') ? 'active' : ''; ?>">
+            <a class="nav-link" href="/liste-clients/1/">Liste des clients</a>
+          </li>
+          <li class="nav-item <?= ($_SERVER['REQUEST_URI'] === '/ajouter-client/') ? 'active' : ''; ?>">
+            <a class="nav-link" href="/ajouter-client/">Ajouter un clients</a>
+          </li>
 
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
-      <button class="btn btn-outline-light my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
-    </form>
+        </ul>
+        <form action="/recherche/" method="post" class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-2" type="search" placeholder="Rechercher" aria-label="Search" name="search" value="<?= (isset($_GET['query']) && $_GET['query'] !== '') ? $_GET['query'] : ''; ?>">
+          <select class="form-control mr-sm-2" name="row">
+    <?php 
+    $current = isset($_GET['row']) ? $row[$_GET['row']]['value'] : 'RaisonSociale';
+    foreach ($row as $key => $elem) {
+        $selected = ($current === $elem['value']) ? 'selected' : '';
+        echo '<option value="'. $key .'" '.$selected.'>'. $elem['text'] .'</option>';
+    } ?>
+            </select>
+          <button class=" btn btn-outline-light my-2 my-sm-0 " type="submit ">
+            <i class="fas fa-search "></i>
+          </button>
+        </form>
+      </div>
+    </nav>
   </div>
-</nav>
 </div>
-    </div>
